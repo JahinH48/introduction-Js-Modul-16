32d7230998b882f40192374a46c737756af5cdbf
unknown <jahinhasan48@gmail.com>
2021-10-07 13:44:28 +0600

    jisan

---
+++ jisan.js
@@ -0,0 +1,134 @@
+// console.log(565)
+
+// var jisan = [15, 45, 55, 65];
+// console.log(jisan.indexOf(55));
+
+// var jisan = [15, 45, 55, 65];
+// console.log(jisan.length);
+
+// var jisan = [15, 45, 55, 65];
+// console.log(jisan.indexOf(55));
+
+
+// var jisan = [15, 45, 55, 65];
+// jisan.push(66)
+// console.log(jisan);
+
+// var ji = [15, 45, 55, 65];
+// ji.shift()
+// console.log(ji);
+
+// var jis = [15, 45, 55, 65];
+// jis.unshift(23, 12);
+// console.log(jis);
+
+
+// var jisan = [15, 45, 55, 65];
+// jisan.pop()
+// var jahin = jisan.pop();
+// console.log(jahin);
+
+
+// var jisa = [15, 45, 55, 65];
+// jisa.splice(3, 5);
+
+// console.log(jisa);
+
+// 17-5-1 variable array and conditionals 
+
+// var bottonColor = "yellow";
+// var isFull = false;
+
+// array 
+
+// var items = ['bottle', 'mug', 'paper', 'pen']
+// items.indexOf('mug');
+// items.push('envelope');
+// items.push('watch');
+// items.pop();
+
+// Conditionals
+
+
+
+// if (items.length >= 4) {
+//     console.log('you have many peopel');
+
+
+// } else if (items.length == 3) {
+//     console.log("you only have one hali items");
+
+// }
+// else {
+//     console.log('wow ! ');
+// }
+
+
+
+
+
+// 17-5-2 While loop , debug and understand while loop
+
+
+// var roastGiven = 0;
+
+// while (roastGiven < 7) {
+
+//     console.log("Roas den , please !");
+//     roastGiven++;
+//     console.log(roastGiven);
+// }
+
+//  17-5-3  More while loops , odd numbers , even numbers
+
+
+// var number = 1;
+
+
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
+
+
+
+// bijor sonkha 
+
+
+// while (number <= 20) {
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// even number 
+
+/*var number = 0;
+
+
+while (number <= 20) {
+    console.log(number);
+    number = number + 2;
+}
+*/
+
+
+//  17-5-4 For loop , how for loop works , while vs for loop
+
+
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+
+//     console.log("Roast den , please !");
+// }
+
+
+
+// 17-5-5  Recap loop 
+
+var iteams = ['bottle', 'book', 'watch', 'changer', 'blog', 'Clan ID', 'Other',];
+
+for (var i = 0; i < iteams.length; i++) {
+    var iteam = iteams[i];
+    console.log(iteam);
+}
+
+
